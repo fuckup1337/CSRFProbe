815d4d715f742077a2e9224368aac6bdd1feec27
@_tID <robinsonpinaxx2000@gmail.com>
2018-04-11 17:26:03 +0530

    Rewrite code

---
+++ csrfprobe.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-:-:-:-:-:-:#
+#    TIDoS Framework     #
+#-:-:-:-:-:-:-:-:-:-:-:-:#
+
+#Author: the-Infected-Drake (@_tID)
+#This module requires TIDoS Framework
+#https://github.com/the-Infected-Drake/TIDoS-Framework 
+
+import sys
+sys.path.append('core/')
+from impo import *
+from globalvars import *
+
+csrf_main()
+
+
