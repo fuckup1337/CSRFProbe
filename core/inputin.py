7db63817d1de6bbb494a7db3828ff64c51f74407
@_tID <robinsonpinaxx2000@gmail.com>
2018-04-11 17:24:20 +0530

    Removed bug

---
+++ core/inputin.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+#coding: utf-8
+
+#-:-:-:-:-:-:-::-:-:#
+#    XSRF Probe     #
+#-:-:-:-:-:-:-::-:-:#
+
+#Author: the-Infected-Drake (@_tID)
+#This module requires XSRF-Probe
+#https://github.com/the-Infected-Drake/XSRF-Probe
+
+from colors import *
+
+def inputin():
+
+	web = raw_input(C+' [$] Enter target address :> '+G)
+
+	if 'http' not in web:
+		web = 'http://' + web
+	if web.endswith('/'):
+		return web
+	else:
+		web = web + '/'
+		return web
